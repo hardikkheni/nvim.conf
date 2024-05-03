88c2e9d13441584b3a2d0b8e1b58bbe80af8da8e
Hardik Kheni <hardikkheni.work@gmail.com>
2024-05-04 02:13:56 +0530

    added git signs

--- lua/hardikkheni/lazy.lua
+++ lua/hardikkheni/lazy.lua
@@ -14,6 +14,20 @@ require('lazy').setup {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
   -- Theme
   {
     'ellisonleao/gruvbox.nvim',
