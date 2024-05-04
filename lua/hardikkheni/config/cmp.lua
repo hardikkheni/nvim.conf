3a704da96815a1593922411cb59a0b9a228a0cc3
Hardik Kheni <hardikkheni.work@gmail.com>
2024-05-04 11:35:00 +0530

    change in cmp.

--- lua/hardikkheni/config/cmp.lua
+++ lua/hardikkheni/config/cmp.lua
@@ -9,7 +9,7 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
-  completion = { completeopt = 'menu,noselect,noinsert' },
+  completion = { completeopt = 'menu,menuone,noinsert' },
 
   -- For an understanding of why these mappings were
   -- chosen, you will need to read `:help ins-completion`
--- lua/hardikkheni/config/lsp.lua
+++ lua/hardikkheni/config/lsp.lua
@@ -129,7 +129,7 @@ local servers = {
   --    https://github.com/pmizio/typescript-tools.nvim
   --
   -- But for many setups, the LSP (`tsserver`) will work just fine
-  -- tsserver = {},
+  tsserver = {},
   --
 
   lua_ls = {
@@ -161,6 +161,8 @@ require('mason').setup()
 local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
   'stylua', -- Used to format Lua code
+  'gofumpt',
+  'goimports',
 })
 require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
